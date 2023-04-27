efe01ce838c9313b83d395bae62e5ff520b52444
Nikolas Powell <powel253@msu.edu>
2023-04-27 19:31:19 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -235,7 +235,7 @@ function App() {
             <p className='fifthText fs-3 text-start ms-5 mt-3'>
               Our Selection:
             </p>
-            <Row sx={1} lg={4}>
+            <Row sm={1} lg={4}>
             <Col>
               <RecordOutput />
             </Col>
